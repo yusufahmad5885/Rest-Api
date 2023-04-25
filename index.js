2e603b623620cc820ab6f34ed3aaf7df7ac1ad9f
Yusuf <yusufahmad5885@gmail.com>
2023-04-25 09:42:47 -0700

    added nodemon, created enpoint for put

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json()); //parse every json
 
 let userList = [
   {
+    id: 1,
     name: "Pedro",
     age: 19,
     married: false,
   },
   {
+    id: 2,
     name: "Paulo",
     age: 20,
     married: false,
   },
   {
+    id: 3,
     name: "Jennifer",
     age: 28,
     married: true,
@@ -21,6 +25,22 @@ let userList = [
 ];
 
 app.get("/users", (req, res) => {
+  res.json(userList);
+});
+
+app.post("/users", (req, res) => {
+  //get data from client, update data in list
+  //return the new list
+  const newUser = req.body;
+  userList.push(newUser);
+  res.json(userList);
+});
+
+app.put("/users", (req, res) => {
+  const newName = req.body.newName;
+  for (let i = 0; i < userList.length; i++) {
+    userList[i].name = newName;
+  }
   res.json(userList);
 });
 
