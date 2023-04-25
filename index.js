e947adbca5c4b82409ad7572b4d381df509e36bd
Yusuf <yusufahmad5885@gmail.com>
2023-04-25 09:10:59 -0700

    added get endpoint, created sample data to mimic database

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const express = require("express");
 
 const app = express();
 
+let userList = [
+  {
+    name: "Pedro",
+    age: 19,
+    married: false,
+  },
+  {
+    name: "Paulo",
+    age: 20,
+    married: false,
+  },
+  {
+    name: "Jennifer",
+    age: 28,
+    married: true,
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.json(userList);
+});
+
 app.listen("3001", () => {
   console.log("Server running on port 3001");
 });
